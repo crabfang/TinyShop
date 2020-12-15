c140f4c195b8baa621bb8db47d6ac4431f746a62
沈建芳 <shenjianfang@u51.com>
2020-12-15 09:06:12 +0800

    [MDF]更新限制条件

--- common/models/product/Product.php
+++ common/models/product/Product.php
@@ -204,7 +204,7 @@ class Product extends \common\models\base\BaseModel
             [['name'], 'string', 'max' => 255],
             [['sketch', 'keywords', 'address_name'], 'string', 'max' => 200],
             [['picture', 'video_url'], 'string', 'max' => 255],
-            [['marque', 'barcode', 'picture', 'video_url', 'marketing_type'], 'string', 'max' => 100],
+            [['marque', 'barcode', 'marketing_type'], 'string', 'max' => 100],
             [['unit'], 'string', 'max' => 20],
             [['tags'], 'safe'],
         ];
